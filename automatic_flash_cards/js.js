6f8311a6bb459ed2ba67c16595e9e7e18a9ab550
wTrystan <61588721+wTrystan@users.noreply.github.com>
2024-11-20 16:47:27 +0000

    Update js.js

--- automatic_flash_cards/js.js
+++ automatic_flash_cards/js.js
@@ -3,6 +3,9 @@ let questions = [];
 let JSON_data = {};
 let finalPrompt = '';
 let line='';
+let nameSubject = '';
+
+document.getElementById("download_JSON").disabled = true;
 
 // Prompt if there is a chapter
 promptChapter = 'You act as a professor of {LEVEL} You will review a document, focusing only on the chapter {CHAPTER} and generate {NUM} questions about this chapter. The question will be in one sentence. The answers will be shorter than 15 words if possible. Display explicitly if you take the chapter of the number of pages. You will reply accordingly to this JSON format with the questions in [question], the equivalent answers in [answer] and a brief summary in less in less than 10 words of the subject covered in the [subject] :    { "subject" : [subject],"list":[{"question": [question], "answer": [answer]}}'
@@ -23,18 +26,28 @@ function generatePrompt() {
     else {
         finalPrompt = finalPrompt + ' {CHAPTER} = ' + document.getElementById("num_chap").value + ' ' + promptChapter;
     }
-
-    //document.getElementById("generated_prompt").innerText = finalPrompt;
 }
 
 async function copyToClipboard() {
     const textToCopy = finalPrompt;
     try {
         await navigator.clipboard.writeText(textToCopy);
-        alert('The prompt has been copied on your clipboard.')
     } catch (err) {
         console.error("Failed to copy text: ", err);
     }
+}
+
+async function pasteClipboard() {
+    const text = await navigator.clipboard.readText();
+    try {
+        document.getElementById("JSON_data").value = text;
+    } catch (err) {
+        console.error("Failed to paste text: ", err);
+    }
+}
+
+function clickPaste() { 
+      pasteClipboard();
 }
 
 function generateAndCopy() {
@@ -43,7 +56,15 @@ function generateAndCopy() {
 }
 
 function updateExample1() {
-    updateQuestions('/assets/questions.json');
+    updateQuestions('/assets/Introduction_to_Large_Language_Models.json');
+}
+
+function updateExample2() {
+    updateQuestions('/assets/Introduction_to_Prompt_Engineering.json');
+}
+
+function updateExample3() {
+    updateQuestions('/assets/Introduction_to_Generative_AI_Agents.json');
 }
 
 function updateQuestions(text) {
@@ -66,9 +87,10 @@ function updateQuestions(text) {
         }
 
 // Fonction pour charger les questions depuis le fichier JSON
-function loadQuestions() { 
+function loadQuestions(text_json) { 
     // Load the data of the JSON file 
-    let JSON_data = document.getElementById("JSON_data").value; 
+    //let JSON_data = document.getElementById(text_json).value; 
+    let JSON_data = text_json; 
     try { 
         JSON_data = JSON.parse(JSON_data);
         // Processing the JSON data directly 
@@ -81,6 +103,26 @@ function loadQuestions() {
          } 
          catch (error) { console.error('Erreur lors du chargement du fichier JSON :', error); } 
         }
+
+function updateNameSubject() {
+    let JSON_data = document.getElementById("JSON_data").value; 
+    try { 
+        JSON_data = JSON.parse(JSON_data);
+        // Processing the JSON data directly 
+        let data = JSON_data;
+        line = data.list.map(item => item); 
+        nameSubject = data.subject.replace(/ /g,'_') + '.json';
+    } 
+    catch (error) { console.error('Erreur lors du chargement du fichier JSON :', error); } 
+}
+
+function firstLoad() {
+    loadQuestions(document.getElementById("JSON_data").value);
+}
+
+function ownLoad() {
+    loadQuestions(document.getElementById("contents_JSON").innerText);
+}
 
 
 // Fonction pour afficher la question actuelle
@@ -115,12 +157,27 @@ function resetJSON() {
     document.getElementById("JSON_data").value = '';
 }
 
+function downloadJSON() {
+    const link = document.createElement("a");
+    const content = document.getElementById("JSON_data").value;
+    const file = new Blob([content], { type: 'application/json' });
+    link.href = URL.createObjectURL(file);
+    updateNameSubject();
+    link.download = nameSubject;
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
+
 
 // Ajouter un gestionnaire d'événement de clic au bloc de contenu
 document.getElementById("button_click_next").addEventListener("click", QorA);
 document.getElementById("button_click_back").addEventListener("click", previousQuestion);
 document.getElementById("button_click_answer").addEventListener("click", displayAnswer);
-document.getElementById("set_data_JSON").addEventListener("click", loadQuestions);
+document.getElementById("set_data_JSON").addEventListener("click", firstLoad);
 document.getElementById("creation_prompt").addEventListener("click", generateAndCopy);
-document.getElementById('reset_JSON').addEventListener('click', resetJSON); 
+document.getElementById('reset_JSON').addEventListener('click', resetJSON);
+document.getElementById('download_JSON').addEventListener('click', downloadJSON); 
+document.getElementById('pasteClip').addEventListener('click', clickPaste); 
 document.getElementById('load_JSON_ex_1').addEventListener('click', updateExample1); 
+document.getElementById('load_JSON_ex_2').addEventListener('click', updateExample2); 
+document.getElementById('load_JSON_ex_3').addEventListener('click', updateExample3); 
